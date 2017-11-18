11dcfe1e4f701341c50c0a17098adb17d05084ad
Gregory Neal <gnealsr@gmail.com>
2017-11-18 15:11:24 -0600

    React Hello app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import HelloMessage from './Hello';
+import ListFriends from './Friends';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+ReactDOM.render(<HelloMessage name="Greg" />,
+  document.getElementById('root'));
